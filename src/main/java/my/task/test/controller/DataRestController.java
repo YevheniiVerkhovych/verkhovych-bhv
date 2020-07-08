e2734c217bb853ac9086d57d0382e6ab0ed65672
AGV <verhovich@gmail.com>
2020-07-08 18:40:02 +0300

    7/8/2020 18.40 UTF-8

--- src/main/java/my/task/test/controller/DataRestController.java
+++ src/main/java/my/task/test/controller/DataRestController.java
@@ -3,6 +3,8 @@ package my.task.test.controller;
 import my.task.test.exceptions.DataAlreadyExistException;
 import my.task.test.exceptions.DataNotFoundException;
 import my.task.test.service.DataService;
+
+import org.apache.commons.codec.binary.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,60 +30,67 @@ public class DataRestController {
 	@GetMapping("/data")
 	public String getData(@RequestParam() String key) {
 
-		if (key.equals(null)) 
+		if (key.equals(null))
 			throw new RuntimeException("Key is Null");
-		
+
 		String value = dataService.getData(key);
 
-		if (value.contentEquals(null)) 
-			throw new DataNotFoundException("Data key not found - " + key);
-			
-		logger.info("Key: " + key + " for value: " + value);
+		if (value.contentEquals(null))
+			throw new DataNotFoundException("Data key not found - " + toUTF(key));
+
+		logger.info("Key: " + toUTF(key) + " for value: " + toUTF(value));
 		return value;
 	}
 
 	@PostMapping("/data")
 	public String addData(@RequestParam() String key, @RequestParam() String value) {
-		
-		if (key.equals(null)) 
+
+		if (key.equals(null))
 			throw new RuntimeException("Key is Null");
 
-		if (dataService.getData(key) != null ) {			
-			throw new DataAlreadyExistException("Data key already exist - " + key);
-		}			
-		
-		dataService.saveData(key, value);
+		if (dataService.getData(key) != null) {
+			throw new DataAlreadyExistException("Data key already exist - " + toUTF(key));
+		}
 
-		logger.info("Key: " + key + " for value: " + value);
+		dataService.saveData(toUTF(key), toUTF(value));
+
+		logger.info("Key: " + toUTF(key) + " for value: " + toUTF(value));
 		return value;
 	}
 
 	@PutMapping("/data")
 	public void updateData(@RequestParam() String key, @RequestParam() String value) {
-		
-		if (key.equals(null)) 
+
+		if (key.equals(null))
 			throw new RuntimeException("Key is Null");
 
 		if (dataService.getData(key) == null) {
-			throw new DataNotFoundException("Data key not found - " + key);
+			throw new DataNotFoundException("Data key not found - " + toUTF(key));
 		}
-		
-		dataService.updateData(key, value);
+
+		dataService.updateData(toUTF(key), toUTF(value));
 
 	}
 
 	@DeleteMapping("/data")
 	public void deleteData(@RequestParam() String key) {
-		
-		if (key.equals(null)) 
+
+		if (key.equals(null))
 			throw new RuntimeException("Key is Null");
 
 		if (dataService.getData(key) == null) {
-			throw new DataNotFoundException("Data key not found - " + key);
+			throw new DataNotFoundException("Data key not found - " + toUTF(key));
 		}
-		
-		dataService.deleteData(key);
+
+		dataService.deleteData(toUTF(key));
 
 	}
 
+	private String toUTF(String notUtf8String) {
+
+		byte[] bytes = StringUtils.getBytesUtf8(notUtf8String);
+
+		return StringUtils.newStringUtf8(bytes);
+
+	}
 }
